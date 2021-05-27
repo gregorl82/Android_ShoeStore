3db5084b26a7a8e65cef93a107724b07000339fd
Gregor Lennie <gregorlennie@hotmail.co.uk>
2021-05-27 20:23:14 +0100

    Inflate fragment views with databinding

--- app/src/main/java/com/udacity/shoestore/screens/shoedetail/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/screens/shoedetail/ShoeDetailFragment.kt
@@ -5,15 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
 class ShoeDetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentShoeDetailBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_shoe_detail, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_detail, container, false)
+        return binding.root
     }
 }

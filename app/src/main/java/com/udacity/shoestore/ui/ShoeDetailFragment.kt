9e1170f6dd3045ff921ee35887983dbed1a5acb5
Gregor Lennie <gregorlennie@hotmail.co.uk>
2021-05-29 18:28:25 +0100

    Adds shoe detail fragment layout

--- app/src/main/java/com/udacity/shoestore/ui/ShoeDetailFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/ShoeDetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeDetailBinding
 
@@ -18,6 +19,15 @@ class ShoeDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_detail, container, false)
+
+        binding.cancelButton.setOnClickListener {
+            findNavController().navigate(R.id.action_shoeDetailFragment_to_shoeListFragment)
+        }
+
+        binding.saveButton.setOnClickListener {
+            findNavController().navigate(R.id.action_shoeDetailFragment_to_shoeListFragment)
+        }
+
         return binding.root
     }
 }

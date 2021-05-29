97a8a2feaae05621f92bdaa7be2e1f251a109eca
Gregor Lennie <gregorlennie@hotmail.co.uk>
2021-05-29 17:19:52 +0100

    Adds shoe list screen with navigation

--- app/src/main/java/com/udacity/shoestore/ui/InstructionFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/InstructionFragment.kt
@@ -1,10 +1,8 @@
 package com.udacity.shoestore.ui
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
@@ -27,4 +25,5 @@ class InstructionFragment : Fragment() {
 
         return binding.root
     }
+
 }
--- app/src/main/java/com/udacity/shoestore/ui/ShoeListFragment.kt
+++ app/src/main/java/com/udacity/shoestore/ui/ShoeListFragment.kt
@@ -1,11 +1,10 @@
 package com.udacity.shoestore.ui
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListBinding
 
@@ -18,6 +17,26 @@ class ShoeListFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_shoe_list, container, false)
+
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_shoeListFragment_to_shoeDetailFragment)
+        }
+
+        setHasOptionsMenu(true)
+
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.shoe_list_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId === R.id.action_logout) {
+            findNavController().navigate(R.id.action_shoeListFragment_to_loginFragment)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }

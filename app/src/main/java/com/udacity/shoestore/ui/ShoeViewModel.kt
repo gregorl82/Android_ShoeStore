f459e7e79d4ace57b27fc014f9e9bc6bcf567fe0
Gregor Lennie <gregorlennie@hotmail.co.uk>
2021-05-30 21:35:35 +0100

    Add two way databinding on double value

--- app/src/main/java/com/udacity/shoestore/ui/ShoeViewModel.kt
+++ app/src/main/java/com/udacity/shoestore/ui/ShoeViewModel.kt
@@ -14,39 +14,6 @@ class ShoeViewModel : ViewModel() {
 
 
     init {
-        _shoeList.value = mutableListOf(
-            Shoe(
-                name = "Air Max",
-                company = "Nike",
-                size = 9.5,
-                description = "Latest stylish sneakers"
-            ),
-            Shoe(
-                name = "Crossfit",
-                company = "Nike",
-                size = 7.5,
-                description = "Wear these to look good at the gym"
-            ),
-            Shoe(
-                name = "Stan Smith",
-                company = "Adidas",
-                size = 8.0,
-                description = "Inspired by a tennis legend"
-            ),
-            Shoe(
-                name = "Classic",
-                company = "Reebok",
-                size = 10.0,
-                description = "As the name suggests, classic!"
-            ),
-            Shoe(
-                name = "Adrenaline 21 GTS",
-                company = "Brooks",
-                size = 11.0,
-                description = "Running shoes for the serious athlete"
-            )
-        )
-
         Timber.i("ShoeViewModel created")
     }
 
